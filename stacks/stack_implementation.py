c22684cafed6838f64bd115d8f050159ab76912e
tverg419 <tverg419@gmail.com>
2021-11-27 22:03:12 -0500

    stack implementation

--- stacks/stack_implementation.py
+++ stacks/stack_implementation.py
@@ -1,26 +1,46 @@
 class Stack:
 
+    # initializes a Stack 
     def __init__(self):
         self.stack = []
     
+    # adds a new item to the top of the stack
     def push(self, value):
-        pass
-
+        self.stack.append(value)
+        print(f'{value} was added to the stack')
+    
+    # removes and returns the last item in a stack
     def pop(self):
-        pass
+        return self.stack.pop()
 
-    def peel(self):
-        pass
+    # returns the top of the stack 
+    def peek(self):
+        return self.stack[len(self.stack) - 1]
 
+    # returns if the stack is empty
     def isEmpty(self):
-        pass
+        return len(self.stack) == 0
 
+    # returns the length of the stack
     def size(self):
-        pass
+        return len(self.stack)
 
-    def generate(self):
-        pass
+    # takes a list as an input and adds it to the stack
+    def generate(self, nums):
+
+        for num in nums:
+            self.push(num)
 
 if __name__ == "__main__":
     s = Stack()
+    print(s.isEmpty())
+    s.generate([1, 2, 3])
+    print(s.peek())
+    
+    print(s.isEmpty())
+    s.pop()
+    s.pop()    
+    s.pop()    
+    print(s.isEmpty())
+    s.push(4)
     print("Working")
